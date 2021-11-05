458af2f2b63eb3815966fce9704d901a5ffaaceb
Thiago Faria Ramos <62444689+ThFramos@users.noreply.github.com>
2021-11-04 23:17:13 -0300

    Finalização Jogo Dinossauro

---
+++ js/script.js
@@ -0,0 +1,82 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+const points = document.querySelector('.count-points');
+
+
+
+let isJumping = false;
+let isGameOver = false;
+let position = 0;
+let pointsValue = 0;
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32) {
+    if (!isJumping) jump();
+  }
+}
+
+function jump() {
+  isJumping = true;
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      // Descendo
+      clearInterval(upInterval);
+
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+          isJumping = false;
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 20);
+    } else {
+      // Subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
+}
+
+function addPoints() {
+  pointsValue += 10;
+
+  points.innerHTML = `Pontos: ${pointsValue}`
+
+}
+
+function createCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  if (isGameOver) return;
+
+  cactus.classList.add('cactus');
+  background.appendChild(cactus);
+  cactus.style.left = cactusPosition + 'px';
+
+  let leftTimer = setInterval(() => {
+    if (cactusPosition < -60) {
+      // Saiu da tela
+      clearInterval(leftTimer);
+      background.removeChild(cactus);
+      addPoints();
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      // Game over
+      clearInterval(leftTimer);
+      isGameOver = true;
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
+}
+points.innerHTML = `Pontos: ${pointsValue}`
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
